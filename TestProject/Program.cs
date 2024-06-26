953687f1c6442d0a55d8297e8366df32e63d545a
Riccardo Foti <riccardofoti97@gmail.com>
2024-06-26 13:44:58 +0100

    Initial commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    The following lines of code reverse
+    a string and count the number of times
+    the letter "o" appears.
+*/
+
+string initialMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = initialMessage.ToCharArray();
+Array.Reverse(charMessage);
+int letterCount = 0;
+
+foreach (char i in charMessage)
+{
+    if (i == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string reversedMessage = new String(charMessage);
+
+Console.WriteLine(reversedMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
