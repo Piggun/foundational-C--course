dad05f273c1fd8e841df3bcb161436b1672a9f0e
Riccardo Foti <riccardofoti97@gmail.com>
2024-07-12 16:32:23 +0100

    Complete Control variable scope and logic using code blocks section

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,13 +1,15 @@
-﻿string permission = "Admin";
-int level = 55;
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
-if (permission.Contains("Admin") && level > 55)
-    Console.WriteLine("Welcome, Super Admin User.");
-else if (permission.Contains("Admin") && level <= 55)
-    Console.WriteLine("Welcome, Admin user.");
-else if (permission.Contains("Manager") && level >= 20)
-    Console.WriteLine("Contact an Admin for acccess.");
-else if (permission.Contains("Manager") && level < 20)
-    Console.WriteLine("You do not have sufficient privileges.");
-else
-    Console.WriteLine("You do not have sufficient privileges.");
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
