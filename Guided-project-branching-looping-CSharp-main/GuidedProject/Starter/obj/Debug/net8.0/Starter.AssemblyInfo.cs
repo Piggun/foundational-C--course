8172946cb9bdaad4e92d84208cde06ec15d94a3c
Riccardo Foti <riccardofoti97@gmail.com>
2024-07-19 15:59:22 +0100

    Complete 'Develop conditional branching and looping structures' section

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4dc64c0de64a1212c1404ad0e82f758fa5537d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0835ef9f199ed377321547b854fea235870158c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
