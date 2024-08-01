70ec418d6628c3323da0212f11f0439d0e6d5a09
Riccardo Foti <riccardofoti97@gmail.com>
2024-08-01 11:26:20 +0100

    Complete 'Convert data types using casting and converstion techniques' section

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -80,21 +80,41 @@
 //     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-string message = "";
-float total = 0;
-float result;
-foreach (string value in values)
-{
-    if (float.TryParse(value, out result))
-    {
-        total += result;
-    }
-    else
-    {
-        message += value;
-    }
-}
+// string message = "";
+// float total = 0;
+// float result;
+// foreach (string value in values)
+// {
+//     if (float.TryParse(value, out result))
+//     {
+//         total += result;
+//     }
+//     else
+//     {
+//         message += value;
+//     }
+// }
 
-Console.WriteLine($"Message: {message} \nTotal: {total}");
+// Console.WriteLine($"Message: {message} \nTotal: {total}");
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+
+int result1 = Convert.ToInt32(value1 / value2);
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / (float)value1;
+
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
