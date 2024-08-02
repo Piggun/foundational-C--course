366c297ed0b105cbc60320df64a70f53a8bfb2ce
Riccardo Foti <riccardofoti97@gmail.com>
2024-08-02 16:33:06 +0100

    Complete 'Perform operations on arrays using helper methods' section

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -175,15 +175,37 @@
 // string result = new string(valueArray);
 // Console.WriteLine(result);
 
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray);
-Console.WriteLine(result);
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
 
-string[] items = result.Split(',');
-foreach (string item in items)
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string[] wordsArr = pangram.Split(" ");
+// foreach (var word in wordsArr)
+// {
+//     char[] charArr = word.ToCharArray();
+//     Array.Reverse(charArr);
+//     string result = new string(charArr);
+//     Console.Write($"{result} ");
+// }
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] ordersArr = orderStream.Split(",");
+Array.Sort(ordersArr);
+foreach (var order in ordersArr)
 {
-    Console.WriteLine(item);
+    if (order.Length != 4)
+        Console.WriteLine($"{order}\t- Error");
+    else
+        Console.WriteLine(order);
 }
