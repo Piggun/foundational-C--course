cc9207ef60f478f02859720aa1500a07891935af
Riccardo Foti <riccardofoti97@gmail.com>
2024-08-02 13:55:35 +0100

    Complete unit 4 of 'Perform operations on arrays using helper methods' section

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -99,22 +99,91 @@
 
 // Console.WriteLine($"Message: {message} \nTotal: {total}");
 
-int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+// int value1 = 11;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
 
-// Your code here to set result1
+// // Your code here to set result1
 
-int result1 = Convert.ToInt32(value1 / value2);
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// int result1 = Convert.ToInt32(value1 / value2);
+// // Hint: You need to round the result to nearest integer (don't just truncate)
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-// Your code here to set result2
-decimal result2 = value2 / (decimal)value3;
+// // Your code here to set result2
+// decimal result2 = value2 / (decimal)value3;
 
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-// Your code here to set result3
-float result3 = value3 / (float)value1;
+// // Your code here to set result3
+// float result3 = value3 / (float)value1;
 
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// string result = new string(valueArray);
+// Console.WriteLine(result);
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
