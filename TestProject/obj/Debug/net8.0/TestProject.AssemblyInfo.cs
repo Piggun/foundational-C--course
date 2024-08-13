ec17c1b2e85d886212955de5139f470c07f55929
Riccardo Foti <riccardofoti97@gmail.com>
2024-08-13 17:44:17 +0100

    Complete 'Create methods that return values' section

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9966140e140296d05ce9aafc5f1288b18b1cfa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51a5064922b5697a04d19e783d865b4665997b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
